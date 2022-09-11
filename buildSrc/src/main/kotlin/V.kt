a315b989f15069558910338e25d618363c2aa37c
FlyInWind <2518509078@qq.com>
2022-09-12 01:05:30 +0800

    update buildSrc

--- buildSrc/src/main/kotlin/V.kt
+++ buildSrc/src/main/kotlin/V.kt
@@ -7,20 +7,20 @@ object V {
 
     const val jjwt = "0.11.2"
 
+    const val lombokPlugin = "6.5.0.3"
+
     const val reactorKotlinExtensions = "1.0.2.RELEASE"
 
-    const val springBoot = "2.6.3"
+    const val springBoot = "2.7.2"
     const val springCloud = "3.0.3"
-    const val springCloudDependencies = "2020.0.3"
+    const val springCloudDependencies = "2021.0.3"
     const val springCloudAlibaba = "2021.1"
 //    const val springDoc = "1.4.1"
-
-    const val springSecurityOauth2AuthorizationServer = "0.2.2"
 
     const val mapstruct = "1.4.2.Final"
     const val mariaJdbc = "2.6.1"
     const val mybatisPlus = "3.4.3.1"
 
-    const val kotlin = "1.6.10"
+    const val kotlin = "1.7.10"
     const val kotlinLogging = "2.1.21"
 }
